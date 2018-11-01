288dc27d6a61a526352808c193f4673f6eb95cde
Masood Ahmed Rafay <rafayqureshi2010@gmail.com>
2018-11-01 16:43:01 +0100

    trial commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function Data() {
 }
 
 /*
-  Adds an order to to the queue
+  Adds an order to the queue
 */
 Data.prototype.addOrder = function (order) {
   //Store the order in an "associative array" with orderId as key
