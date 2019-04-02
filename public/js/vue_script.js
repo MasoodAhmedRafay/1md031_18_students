29a19be7c8fe6cf1c92ebcfa4e6c46b85c91fdcc
MasoodAhmedRafay <masood.rafay.6789@student.uu.se>
2019-04-02 16:57:10 +0200

    Part 5 "Messaging tasks" Done

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,92 +1,46 @@
-/*var food = [
-	{
-		"name": "The Fire Burger",
-		"kCal": 750,
-		"lactose": true,
-		"gluten": true,
-		"stock": 2,
-		"checked":false,
-		"img": "/img/TheFireBurger.jpg"
-	},
-	{
-		"name": "Fried Turkey Burger",
-		"kCal": 600,
-		"lactose": true,
-		"gluten": false,
-		"stock": 3,
-		"checked":false,
-		"img": "/img/friedturkeyburger.jpg"
-	},
-	{
-		"name": "A Double w/ Cheese",
-		"kCal": 1800,
-		"lactose": true,
-		"gluten": true,
-		"stock": 1,
-		"checked":false,
-		"img": "/img/Double-Cheeseburger.jpg"
-	}
-];*/
-/*
-var myData=[];
-myData = [
-	{
-		"name": "The Fire Burger",
-		"kCal": 750,
-		"lactose": true,
-		"gluten": true,    
-		"stock": 2,
-		"checked":false,
-		"img": "/img/TheFireBurger.jpg"
-	},
-	{
-		"name": "Fried Turkey Burger",
-		"kCal": 600,
-		"lactose": true,
-		"gluten": false,    
-		"stock": 3,
-		"checked":false,
-		"img": "/img/friedturkeyburger.jpg"
-	},
-	{
-		"name": "A Double w/ Cheese",
-		"kCal": 1800,
-		"lactose": true,
-		"gluten": true,   
-		"stock": 1, 
-		"checked":false,
-		"img": "/img/Double-Cheeseburger.jpg"
-	}
-];
- //myData=JSON.parse(posData);
-*/
-//console.log(myData);
-console.log(food);
-var app = new Vue({
-		el: '#store',
-		data: {
-			//MenuItem:myData,
-			MenuItem:food,
-			orderData:[],
-			userData:'',
-		},
-		methods: {
-	        markDone: function() {
-	            //Add some functionality
-	        },
-	        PlaceOrder () {
-	        	console.log(this.$refs.f_name.value);
-	        	var temp_item={
-                    f_name:this.$refs.f_name.value,
-                    l_name:this.$refs.l_name.value,
-                    email:this.$refs.email.value,
-                    street:this.$refs.street.value,
-                    house:this.$refs.house.value,
-                    sex:this.$refs.sex.value
-                };
-	        	console.log(temp_item);
-                this.userData=temp_item;
-		    }
-	    }
+var socket = io();
 
+var orders = new Vue({
+    el: '#store',
+    data: {
+        MenuItem: food,
+        details: {x: 0, y: 0},
+        lastOrder: 0,
+        orderItems: [],
+        userData: [],
+    },
+    created: function () {
+        socket.on('initialize', function (data) {
+            this.orders = data.orders;
+        }.bind(this));
+    },
+    methods: {
+        getNext: function () {
+            var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+                return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+        },
+
+
+        displayOrder: function(event) {
+            var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                y: event.currentTarget.getBoundingClientRect().top};
+
+            this.details= { x: event.clientX - 10 - offset.x,
+                y: event.clientY - 10 - offset.y };
+
+
+        },
+        addOrder: function () {
+            if (this.orderItems.length!== 0) {
+                socket.emit("addOrder", { orderId: this.getNext(),
+                    details: this.details,
+                    orderItems: this.orderItems,
+                    userItems: this.userData,
+                });
+            }
+        },
+
+    },
 });
